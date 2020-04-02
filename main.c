6e3a68587f39cc163be05a0f97dd171dfd9acae3
Simon Devitt <devitt1@me.com>
2020-04-02 16:20:00 +1100

    Update main.c

--- main.c
+++ main.c
@@ -18,18 +18,36 @@
 
 int main(){
     
-    int qs = 1;
+    int i,j,k,N, qubit;
+    Register reg;
     FILE *out=fopen("out.dat","w");
     clock_t start, end;
     double cpu_time_used;
-    start = clock();
-	
-	
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    fprintf(out, "%d, %f\n", qs, cpu_time_used);
+    
+    for(i=1; i<=30; i++){
+        printf("Qubits = %d\n", i);
+        for(j=1; j<=100; j++){
+        
+            start = clock();
+            initialize_simulator();
+            reg = create_register(i, 0, 0, out);
+            set_state(reg, 0, 1, 0);
+    
+            for(k=0; k<j; k++){
+                ds_yrot(reg,rand() % i,Pi*uniform(),0);
+            }
+            
+            destroy_register(reg);
+    
+            end = clock();
+            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+            fprintf(out, "%d, %d, %f\n", i, j, cpu_time_used);
+        }
+    }
+    
     fclose(out);
     return 0;
+
 }
 
 ;
